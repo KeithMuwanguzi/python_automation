94c89cae6134fc45d207a9db4b0432d64359d47e
Keith Jason <keithmuwanguzi@gmail.com>
2024-01-24 07:27:01 +0300

    Fresh

--- web_scrapping/scrap1.py
+++ web_scrapping/scrap1.py
@@ -1,21 +1,16 @@
 from pydoc import html
-from urllib import response
 import requests
 from bs4 import BeautifulSoup
 
 
 #url to the scrapped site
 url = "https://docs.python.org/3/library/array.html#array.array.append"
-response = requests.get(url)
+res = requests.get(url)
 
 
-if response.status_code == 200:
-    html_content = response.content
+if res.status_code == 200:
+    html_content = res.content
     soup = BeautifulSoup(html_content,'html.parser')
-    
-    # html_data = soup.prettify()
-    # with open("web_data.html",'w') as file:
-    #     file.write(str(html_data))
     
     text= soup.get_text()
     with open('text.txt','w') as text_doc:
