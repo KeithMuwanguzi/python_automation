35c7be2d46a0a1c0da5c6f4d7d23a4d4f4e49eea
Keith Jason <keithmuwanguzi@gmail.com>
2024-01-24 12:52:14 +0300

    Added

--- web_scrapping/scrap1.py
+++ web_scrapping/scrap1.py
@@ -6,14 +6,9 @@ from bs4 import BeautifulSoup
 #url to the scrapped site
 url = "https://docs.python.org/3/library/array.html#array.array.append"
 res = requests.get(url)
-
-
-if res.status_code == 200:
-    html_content = res.content
-    soup = BeautifulSoup(html_content,'html.parser')
+soup = BeautifulSoup(res.text,'html.parser')
     
-    text= soup.get_text()
-    with open('text.txt','w') as text_doc:
-        text_doc.write(text)
-else:
-    print("This website denied you access to scrap it")
+methods = soup.findAll('dl',attrs={'class':'py method'})
+
+for method in methods:
+    print(method.text)
