6ff9b7a619d98b223620a24167bd2737d967eb83
keith <keithmuwanguzi@gmail.com>
2024-01-27 21:17:22 +0300

    Reminder

--- web_scrapping/scrap4.py
+++ web_scrapping/scrap4.py
@@ -7,5 +7,7 @@ res = requests.get(url)
 
 if res.status_code == 200:
     print("Continue with this")
+    soup = BeautifulSoup(res.text,"html.parser")
+    print(soup.prettify())
 else:
     print("Just stop bro")
