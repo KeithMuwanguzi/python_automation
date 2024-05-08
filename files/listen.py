62de854d217931ecb67cc2291773582d28eab4eb
keith <keithmuwanguzi@gmail.com>
2024-05-08 19:59:45 +0300

    Listen

--- files/listen.py
+++ files/listen.py
@@ -8,6 +8,19 @@ def created(event):
 
     print(f'{event.src_path} has been created')
 
+def modified(event):
+    if event.is_directory:
+        print("A folder modified")
+
+    print(f'{event.src_path} has been modified')
+
+def deleted(event):
+    if event.is_directory:
+        print("A folder created")
+
+    print(f'{event.src_path} has been deleted')
+
+
 if __name__ == '__main__':
     path = 'D:/code/python/test'
 
@@ -15,6 +28,8 @@ if __name__ == '__main__':
     event_handler = FileSystemEventHandler()
 
     event_handler.on_created = created
+    event_handler.on_deleted = deleted
+    event_handler.on_modified = modified
     observer.schedule(event_handler,path,recursive=True)
 
     observer.start()
